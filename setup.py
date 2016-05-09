7ce9ed3d8c68310693612983366ce34b73257d96
borismarin <borismarin@gmail.com>
2016-05-09 18:06:39 +0100

    more detailed install info

--- setup.py
+++ setup.py
@@ -10,8 +10,18 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(name='geppetto',
       version='0.1',
       description='Python interfaces to the Geppetto Simulation Engine',
+      long_description=long_description,
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Intended Audience :: Science/Research'
+      ],
+      keywords='science simulation biology bio-informatics',
       url='http://www.geppetto.org/',
       author='Boris Marin',
       author_email='borismarin@gmail.com',
       license='MIT',
-      packages=['geppetto'])
+      packages=['geppetto'],
+      install_requires=['toposort'])
