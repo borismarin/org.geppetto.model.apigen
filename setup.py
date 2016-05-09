3e4a0bd6159e8f8e04a4a51cf1049a92be93e942
borismarin <borismarin@gmail.com>
2016-05-09 18:51:26 +0100

    tweaking setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup
-from os import path
 from codecs import open
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = f.read()
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
 
 setup(name='geppetto',
       version='0.1',
       description='Python interfaces to the Geppetto Simulation Engine',
-      long_description=long_description,
+      long_description=readme(),
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Intended Audience :: Science/Research'
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Intended Audience :: Science/Research'
       ],
       keywords='science simulation biology bio-informatics',
       url='http://www.geppetto.org/',
